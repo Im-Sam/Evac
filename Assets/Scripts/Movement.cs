d97e9f7dfc9a693b8103e953f0020aed1de49991
Liam <80212787+Liam-RE@users.noreply.github.com>
2021-11-25 20:33:05 +0000

    Add Gravity + Jump + Sprint - Movement.cs

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,19 +6,47 @@ public class Movement : MonoBehaviour
 {
     public CharacterController controller;
     [Header("values")]
-    public float speed = 10f;
-    public float gravity = -9.81f;
+    public float speed = 12f;
+    public float gravity = -10.18f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.9f;
+    public LayerMask groundMask;
+
     Vector3 velocity;
+    bool isGrounded;
 
     
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -3f;
+        }
+
+        if (Input.GetKey("left shift") && isGrounded)
+        {
+            speed = 20f;
+        }
+        else
+        {
+            speed = 12f;
+        }
+        
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+         if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
 
         velocity.y += gravity * Time.deltaTime; 
 
